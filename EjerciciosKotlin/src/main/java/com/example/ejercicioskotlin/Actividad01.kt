681332d14983eb4461fff98094aafcbb92f4bcf2
WorickMorth9810 <thebatter1098@gmail.com>
2020-10-06 23:38:28 -0500

    Hola, este es mi primer Commit

---
+++ EjerciciosKotlin/src/main/java/com/example/ejercicioskotlin/Actividad01.kt
@@ -0,0 +1,15 @@
+package com.example.ejercicioskotlin
+
+/*
+Actividad01
+Autor: Josue Ivan Alejandro Melendrez Hernandez
+Fecha: 04/09/2020
+
+ */
+
+fun main(args: Array<String>){
+    println("Hola Mundo")
+    println("Desarrollo de Aplicaciones para Dispositivos Moviles")
+    println("Josue Ivan Alejandro Melendrez Hernandez")
+    print("Worick Morth")
+}
